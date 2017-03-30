d85dfc077ff883eb426e1f880a66efa705592435
Anshul Sahni <anshul_sahni@live.com>
2017-03-31 00:30:50 +0530

    added Example component in src

---
+++ src/Example.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Example extends Component {
+  render() {
+    return (
+      <div>Example Component</div>
+    );
+  }
+}
+
+export default Example;
