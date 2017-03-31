98594d140b6c0badbfcea55985e4cc4f1bf7910f
Anshul Sahni <anshul_sahni@live.com>
2017-03-31 11:24:24 +0530

    example tests

--- test/Example.test.js
+++ test/Example.test.js
@@ -1,14 +1,14 @@
 const React = require('react');
-const enzyme = require('enzymy');
+const enzyme = require('enzyme');
 const expect = require('chai').expect;
 
 const shallow = enzyme.shallow;
 
 const Example = require('../src/Example.jsx').default;
 
-describe('<Example />', function() {
-  it('should render component correctly', function() {
-    const wrapper = shallo(<Example />);
+describe ('<Example />', function() {
+  it ('should render component correctly', function() {
+    const wrapper = shallow(<Example />);
     expect(wrapper.find('div')).to.have.length(1);
   });
 });
