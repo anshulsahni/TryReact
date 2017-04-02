a43369ead5acbf86302ed8e54714a378d8405e39
Anshul Sahni <anshul_sahni@live.com>
2017-04-03 01:06:59 +0530

    Family Tree initial example created

--- example/scripts/Root.jsx
+++ example/scripts/Root.jsx
@@ -2,12 +2,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Example from '../../src';
+import FamilyTree from '../../src';
+
+const sampleFamily = {
+  id: 1,
+  name: 'Jagdish Chandra Sahni',
+  dob: '29-04-1930',
+  children: [
+    {
+      id: 2,
+      name: 'Bharat Bhushan Sahni',
+      dob: '27-09-1963',
+      children: [
+        {
+          id: 3,
+          name: 'Anshul Sahni',
+          dob: '17-09-1993',
+          children: [],
+        },
+        {
+          id: 4,
+          name: 'Arpit Sahni',
+          dob: '29-06-1998',
+          children: [],
+        },
+      ],
+    },
+  ],
+};
 
 class Root extends React.Component {
   render() {
     return (
-      <Example />
+      <FamilyTree family={sampleFamily} />
     );
   }
 }
