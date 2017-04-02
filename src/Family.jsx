fd08932a3c2c358bf7bb9596b93a88ffe9e7e3fc
Anshul Sahni <anshul_sahni@live.com>
2017-04-03 00:42:14 +0530

    Family component added

---
+++ src/Family.jsx
@@ -0,0 +1,52 @@
+import React, { Component, PropTypes } from 'react';
+
+import map from 'lodash.map';
+import head from 'lodash.head';
+
+import Member from './Member';
+
+const availableModes = [
+  'open',
+  'close',
+];
+
+class Family extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: head(availableModes),
+    };
+  }
+
+  renderChildren() {
+    return map(this.props.member.children, child => (
+      <Family member={child} />
+    ));
+  }
+
+  render() {
+    const member = this.props.member;
+    return (
+      <div>
+        <Member
+          id={member.id}
+          name={member.name}
+          image={member.image}
+          dob={member.dob}
+          dod={member.dod}
+        />
+        {this.renderChildren()}
+      </div>
+    );
+  }
+}
+
+Family.propTypes = {
+  member: PropTypes.object,
+};
+
+Family.defaultProps = {
+  member: {},
+};
+
+export default Family;
