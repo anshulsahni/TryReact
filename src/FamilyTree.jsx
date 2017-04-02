7e35e59394bdd76e14f3f4c305d3f7df8c3c999b
Anshul Sahni <anshul_sahni@live.com>
2017-04-03 00:45:32 +0530

    required lodash modules added

---
+++ src/FamilyTree.jsx
@@ -0,0 +1,49 @@
+import React, { Component, PropTypes } from 'react';
+
+import Family from './Family';
+
+class FamilyTree extends Component {
+  constructor() {
+    super();
+    this.handleSave = this.handleSave.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSave() {
+    this.props.onSave();
+    // TODO
+  }
+
+  handleDelete() {
+    this.props.onDelete();
+    // TODO
+  }
+
+  handleChange() {
+    this.props.onChange();
+    // TODO
+  }
+
+  render() {
+    return (
+      <Family family={this.props.family} />
+    );
+  }
+}
+
+FamilyTree.propTypes = {
+  family: PropTypes.object,
+  onSave: PropTypes.func,
+  onDelete: PropTypes.func,
+  onChange: PropTypes.func,
+};
+
+FamilyTree.defaultProps = {
+  family: {},
+  onSave: () => {},
+  onDelete: () => {},
+  onChange: () => {},
+};
+
+export default FamilyTree;
