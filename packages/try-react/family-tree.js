46e381919f16d3a2f347c623c9690d9e6f690e1e
Anshul Sahni <anshul_sahni@live.com>
2017-06-08 12:41:33 +0530

    added password-input and family-tree

---
+++ packages/try-react/family-tree.js
@@ -0,0 +1,1 @@
+module.exports = require('try-react-family-tree');
---
+++ packages/try-react/index.js
@@ -0,0 +1,10 @@
+const PasswordInput = require('try-react-password-input');
+const FamilyTree = require('try-react-family-tree');
+
+exports.PasswordInput = PasswordInput;
+exports.FamilyTree = FamilyTree;
+
+module.exports = {
+  PasswordInput,
+  FamilyTree,
+};
